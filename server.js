1e90c2a65ead396965ca8f02b3b8ad8436e8172d
Cooper Griffin <coopergriffin@cmail.carleton.ca>
2024-02-27 10:43:08 -0500

    Updated server.js
    Added comments for future readability

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+//Cooper Griffin 
+//Express main server file and entry point for program 
+//NoteTaker App 
+
+
+//NPM modules that are required 
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -95,7 +101,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
   });
   
-  app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
 
